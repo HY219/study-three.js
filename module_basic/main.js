f893328a0bb7471b8e47b85dcd63e1e660288a1c
hye219 <jhy_0219@daum.net>
2022-09-26 13:20:48 +0900

    js 함수 모듈 모두 가져오기

--- module_basic/main.js
+++ module_basic/main.js
@@ -1,4 +1,6 @@
-import { hello1, hello2 } from './hello.js';
+//import { hello1, hello2 } from './hello.js';
+import * as hello from './hello.js';
+//as -> 어떤 이름으로 가져올 것인지 정해주기
 
-hello1();
-hello2();
+hello.hello1();
+hello.hello2();
