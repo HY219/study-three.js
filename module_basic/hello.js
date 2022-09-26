1e71282b3950c95319c6fee3a956ea2040438ff7
hye219 <jhy_0219@daum.net>
2022-09-26 13:37:40 +0900

    three.js project 사용할 모듈 방식

--- module_basic/hello.js
+++ module_basic/hello.js
@@ -1,7 +1,7 @@
 //default -> 기본값
 //default 중복 설정 x
 //{hello1}로 모듈 불러오면 error (중괄호 없이 사용하기)
-export default function hello1(){
+export function hello1(){
     console.log("Hello 1!");
 }
 
--- module_basic/main.js
+++ module_basic/main.js
@@ -1,6 +1,6 @@
 //import { hello1, hello2 } from './hello.js';
-//import * as hell from './hello.js'; //as -> 어떤 이름으로 가져올 것인지 정해주기
-import hello1 from './hello.js';
+import * as hell from './hello.js'; //as -> 어떤 이름으로 가져올 것인지 정해주기
+//import hello1 from './hello.js';
 
-hello1();
-//hell.hello2();
+hell.hello1();
+hell.hello2();
